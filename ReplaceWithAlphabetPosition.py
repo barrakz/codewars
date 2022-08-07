b661bad0a0496ca2a0433795011bf2efc3c6b29c
barrakz <b.rakuzy@gmail.com>
2022-08-07 13:40:35 +0200

    alphabet solution tab fixed

--- ReplaceWithAlphabetPosition.py
+++ ReplaceWithAlphabetPosition.py
@@ -1,12 +1,12 @@
 def alphabet_position(text):
-        text = text.lower()
-        string = ''
+    text = text.lower()
+    string = ''
 
-        for i in text:
-            if i.isalpha():
-                string += str(ord(i) - 96)
-                string += ' '
-        return string.rstrip()
+    for i in text:
+        if i.isalpha():
+            string += str(ord(i) - 96)
+            string += ' '
+    return string.rstrip()
 
 
 if __name__ == '__main__':
