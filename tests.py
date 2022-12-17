84cebef90d7fdb03a634896e67c08e6f80df770f
barrakz <b.rakuzy@gmail.com>
2022-12-17 10:43:42 +0100

    add twotoone solution and tests

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 from NumberOfPeopleInTheBUs import number
 from ParseNiceInt import get_age
+from TwoToOne import longest
 
 
 def test_numbers():
@@ -12,3 +13,13 @@ def test_parse_int():
     text = "6 years old"
     expected = 6
     assert get_age(text) == expected
+
+
+def test_longest():
+    assert longest("abcdefg", "hijklmnopqrstuvwxyz") == "abcdefghijklmnopqrstuvwxyz"
+    assert longest("abcdefghijklmnopqrstuvwxyz", "abcdefg") == "abcdefghijklmnopqrstuvwxyz"
+    assert longest("abcdefg", "hijklmnopqrstuvwxyzabcdefg") == "abcdefghijklmnopqrstuvwxyz"
+    assert longest("abcd", "efgh") == "abcdefgh"
+    assert longest("abcd", "dcba") == "abcd"
+    assert longest("abcd", "") == "abcd"
+    assert longest("", "") == ""
